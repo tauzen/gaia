e40458e0b9705b4d66e2ae5deeacaa45d3abab6b
Krzysztof Mioduszewski <kmioduszewski@gmail.com>
2015-02-17 20:24:40 +0100

    enable se pref in gaia
    Change-Id: I92090042f98a090e487db7803005f88ac993d160

--- build/config/phone/custom-prefs.js
+++ build/config/phone/custom-prefs.js
@@ -3,3 +3,4 @@ user_pref('devtools.responsiveUI.customWidth', 320);
 user_pref('devtools.responsiveUI.customHeight', 480);
 user_pref('devtools.responsiveUI.currentPreset', 'custom');
 user_pref('devtools.debugger.forbid-certified-apps', false);
+user_pref('dom.secureelement.enabled', true);
